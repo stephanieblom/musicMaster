62061134491349a9ec48646e5092aa071d00835a
Stephanie Blom <stephblom@hotmail.com>
2021-01-27 21:30:25 -0700

    Setting up API call for search

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import "./App.css";
-import { FormGroup, FormControl, InputGroup} from 'react-bootstrap';
+import { FormGroup, FormControl, InputGroup, Button} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            query: ''
+        }
+    }
+
+    //Function to call API based on your search
+    search(){
+        console.log('this.state', this.state);
+        const BASE_URL = 'https://api.spotify.com/v1/search?';
+        const FETCH_URL = BASE_URL + 'q=' + this.state.query
+                          + '&type=artist&limit=1'
+        console.log('FETCH_URL', FETCH_URL);
+    }
     render(){
         return (
             <div className="App">
@@ -16,8 +31,19 @@ class App extends Component {
                     <InputGroup>
                         <FormControl 
                           type="text" 
-                          placeholder="Search for an Artist"/>
-                          <FontAwesomeIcon icon={faSearch} />
+                          placeholder="Search for an Artist"
+                          value = {this.state.query}
+                          onChange={event => {this.setState({query: event.target.value})}}
+                          onKeyPress={event => {
+                              if (event.key === 'Enter'){
+                                  this.search();
+                              }
+                          }}
+                        />
+                          <Button onClick={()=>this.search()}>
+                              <FontAwesomeIcon icon={faSearch} />
+                          </Button>
+                          
                     </InputGroup>
                 </FormGroup>
                 <div className="Profile">
